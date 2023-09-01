f47fec5d824122de8ba3165675fe0c79c2930cd9
MBBrunito <tecnolastipas@gmail.com>
2023-09-01 12:35:53 -0300

    feat: Tablas relacionadas

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -7,30 +7,48 @@ import {
   DeleteDateColumn,
   OneToOne,
   JoinColumn,
+  OneToMany,
 } from 'typeorm';
 import { Profile } from './profile.entity';
+import { Notice } from 'src/notices/notice.entity';
+import { Comments } from 'src/comments/entities/comments.entity';
 
 @Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
   @Column({ unique: true })
   username: string;
+
   @Column({ unique: true })
   email: string;
+
   @Column()
   password: string;
+
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
+
   @DeleteDateColumn()
   deletedAt: Date; //! PARA BORRADO LOGICO
+
   @Column({ nullable: true })
   authStrategy: string;
+
   @Column({ nullable: true })
   active: boolean;
+
   @Column({ default: 'admin' })
   role: string;
+
   @OneToOne(() => Profile)
   @JoinColumn()
   profile: Profile;
+
+  @OneToMany(() => Notice, (notice) => notice.user)
+  notice: Notice[];
+
+  @OneToMany(() => Comments, (comments) => comments.user)
+  comments: Comments[];
 }

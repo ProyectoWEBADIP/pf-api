8d20a9172538d17e0a5bd39ede3017c3540d6cb5
Gastón <gastibarossi@gmail.com>
2023-09-10 15:58:21 -0300

    [[FEAT]] mercadopago terminado

---
+++ src/payment/payment.service.ts
@@ -0,0 +1,49 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable } from '@nestjs/common';
+import { CreatePaymentDto } from './dto/create-payment.dto';
+import { UpdatePaymentDto } from './dto/update-payment.dto';
+import * as mercadopago from 'mercadopago';
+
+@Injectable()
+export class PaymentService {
+  async createPayment(req: CreatePaymentDto) {
+    const preference = {
+      items: [
+        {
+          title: req.description,
+          unit_price: Number(req.price),
+          quantity: Number(req.quantity),
+        },
+      ],
+      back_urls: {
+        success: 'http://localhost:5173',
+        failure: 'http://localhost:5173',
+        pending: '',
+      },
+    };
+
+    try {
+      const response = await mercadopago.preferences.create(preference);
+      return response;
+    } catch (error) {
+      throw new Error('Error al crear la preferencia de MercadoPago');
+    }
+  }
+
+  findAll() {
+    return `This action returns all payment`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} payment`;
+  }
+
+  update(id: number, updatePaymentDto: UpdatePaymentDto) {
+    return `This action updates a #${id} payment`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} payment`;
+  }
+}

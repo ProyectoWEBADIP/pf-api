b2abe4232905d56ce177d09c45001414d6efecfa
Auliorp <aulioroveroparedes@gmail.com>
2023-09-10 23:30:45 -0300

    trabajando en relaciones uno a uno (user/rol).

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,9 +5,10 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Profile } from './entities/profile.entity';
+import { Rol } from '../roles/entities/rol.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Profile])],
+  imports: [TypeOrmModule.forFeature([User, Profile, Rol])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
